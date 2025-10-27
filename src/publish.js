d6ba4864792be19686c34a4b31d204f6e886b8af
Agrismartchain <jebbarceo@gmail.com>
2025-10-27 11:58:00 +0000

    publisher: switch to Pinata upload (JSON) + SHA-256

--- src/publish.js
+++ src/publish.js
@@ -1,17 +1,30 @@
 /** CREATED BY WAKAMA.farm & Supported by Solana foundation */
 import fs from "fs-extra";
 import crypto from "node:crypto";
-import { Web3Storage, File } from "web3.storage";
+import pinataSDK from "@pinata/sdk";
+import 'dotenv/config.js';
 
-const client = new Web3Storage({ token: process.env.WEB3STORAGE_TOKEN });
+const pinata = new pinataSDK({ pinataJWTKey: process.env.PINATA_JWT });
 const dir = "../wakama-oracle-ingest/batches";
 
-const files = (await fs.readdir(dir)).filter(f=>f.endsWith(".json"));
-for (const f of files) {
-  const p = `${dir}/${f}`;
-  const data = await fs.readFile(p);
-  const sha256 = crypto.createHash("sha256").update(data).digest("hex");
-  const cid = await client.put([new File([data], f)], { wrapWithDirectory:false });
-  console.log(JSON.stringify({ file:f, cid, sha256 }));
-  // TODO J2: emit devnet event via Anchor program
+async function main() {
+  if (!process.env.PINATA_JWT) {
+    console.error("Missing PINATA_JWT in .env");
+    process.exit(1);
+  }
+  const files = (await fs.readdir(dir)).filter(f=>f.endsWith(".json"));
+  for (const f of files) {
+    const p = `${dir}/${f}`;
+    const data = await fs.readFile(p);
+    const sha256 = crypto.createHash("sha256").update(data).digest("hex");
+    const json = JSON.parse(data.toString("utf-8"));
+    const res = await pinata.pinJSONToIPFS(json, {
+      pinataMetadata: { name: f },
+      pinataOptions: { cidVersion: 1 }
+    });
+    const cid = res.IpfsHash;
+    console.log(JSON.stringify({ file:f, cid, sha256 }));
+    // TODO: emit Solana devnet event here (next step)
+  }
 }
+main().catch(e=>{ console.error(e); process.exit(1); });
